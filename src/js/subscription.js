d01e8195c7bfb2326f8eaed3e72af0cfb6c54b1e
Vitalii Shved <vitalyashved@gmail.com>
2022-11-29 11:02:46 +0200

    start subscription

--- src/js/subscription.js
+++ src/js/subscription.js
@@ -6,27 +6,13 @@ const PROMPT_DELAY = 3000;
 const MAX_PROMPT_ATTEMPTS = 3;
 let promptCounter = 0;
 let hasSubscribed = false;
-const modal = new BSN.Modal('#subscription-modal');
 
-openModal();
-
-refs.modal.addEventListener('hide.bs.modal', openModal);
-refs.subscribeBtn.addEventListener('click', onSubscribeBtnClick);
-
-function openModal() {
+const inervalId = setInterval(() => {
   if (promptCounter === MAX_PROMPT_ATTEMPTS || hasSubscribed) {
-    console.log('Максимальное кол-во надоеданий или подписался');
+    console.log('stop interval');
+    clearInterval(inervalId);
     return;
   }
-
-  setTimeout(() => {
-    console.log('Открываем надоедалку');
-    modal.show();
-    promptCounter += 1;
-  }, PROMPT_DELAY);
-}
-
-function onSubscribeBtnClick() {
-  hasSubscribed = true;
-  modal.hide();
-}
+  console.log('Please subscribe - ' + Date.now());
+  promptCounter += 1;
+}, PROMPT_DELAY);
