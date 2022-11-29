1d688133bc2e53a3505e96f30a78603f2b19f9af
VitaliiShv <51060742+VitaliiShv@users.noreply.github.com>
2022-11-29 07:54:52 +0000

    Deploying to gh-pages from @ VitaliiShv/js-playground@ebcd2c189bb0da1e0b393f7401dc2aa473111cbb 🚀

--- notification.07e26d6b.js
+++
@@ -1,2 +0,0 @@
-const i={notification:document.querySelector(".js-alert")};i.notification.addEventListener("click",(function(){i.notification.classList.remove("is-visible")})),i.notification.classList.add("is-visible"),setTimeout((()=>{console.log("закривеємот алерт")}),3e3);
-//# sourceMappingURL=notification.07e26d6b.js.map
--- notification.e3bbe629.js
+++
@@ -1,2 +0,0 @@
-!function(){var i={notification:document.querySelector(".js-alert")};i.notification.addEventListener("click",(function(){i.notification.classList.remove("is-visible")})),i.notification.classList.add("is-visible"),setTimeout((function(){console.log("закривеємот алерт")}),3e3)}();
-//# sourceMappingURL=notification.e3bbe629.js.map
