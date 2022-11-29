bebf2a084950b342c173b7f84c6398a64dff3e04
VitaliiShv <51060742+VitaliiShv@users.noreply.github.com>
2022-11-29 09:03:26 +0000

    Deploying to gh-pages from @ VitaliiShv/js-playground@d01e8195c7bfb2326f8eaed3e72af0cfb6c54b1e 🚀

---
+++ subscription.65d96b8e.js
@@ -0,0 +1,2 @@
+document.querySelector("#subscription-modal"),document.querySelector("button[data-subscribe]");let e=0;const o=setInterval((()=>{if(3===e)return console.log("stop interval"),void clearInterval(o);console.log("Please subscribe - "+Date.now()),e+=1}),3e3);
+//# sourceMappingURL=subscription.65d96b8e.js.map
---
+++ subscription.b90be54e.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector("#subscription-modal"),document.querySelector("button[data-subscribe]");var e=0,o=setInterval((function(){if(3===e)return console.log("stop interval"),void clearInterval(o);console.log("Please subscribe - "+Date.now()),e+=1}),3e3)}();
+//# sourceMappingURL=subscription.b90be54e.js.map
