12e39d826f4619d7501794bc9f208446dda8a1e0
VitaliiShv <51060742+VitaliiShv@users.noreply.github.com>
2022-11-29 07:18:00 +0000

    Deploying to gh-pages from @ VitaliiShv/js-playground@44019a21b96a2850aed6291cbd4bba2839f06f66 🚀

---
+++ notification.07e26d6b.js
@@ -0,0 +1,2 @@
+const i={notification:document.querySelector(".js-alert")};i.notification.addEventListener("click",(function(){i.notification.classList.remove("is-visible")})),i.notification.classList.add("is-visible"),setTimeout((()=>{console.log("закривеємот алерт")}),3e3);
+//# sourceMappingURL=notification.07e26d6b.js.map
